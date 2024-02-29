a74dc5196f800aae3f472f37827fbef416315b8f
William Franzen <wfranzen@mtech.edu>
2024-02-29 01:08:39 -0700

    HW 4

--- HW 4/HW 4.R
+++ HW 4/HW 4.R
@@ -1,33 +1,42 @@
-setwd("~/Documents/College N' Stuff/Senior/The Second Semester/STAT 435/STAT435")
+# Homework 4 Stuff
+
 # Printing fewer or more digits ----
 # Use the digits parameter in the print() function
 print(pi, digits = 4)
 print(pi, digits = 5)
+
 # Use cat() and format()
 cat(format(pi, digits = 5), "\n")
+
 # This will also format vectors
 print(pnorm(-3:3), digits = 2)
+
+
 # Redirecting output to files ----
 # Use cat with the file parameter
 cat("Sonic the Hedgehog!!", file = "sonic.txt")
+# sink() can start sinking output to a file.
+# source() can retrieve the stuff from a file.
+
+
 # Listing files ----
 # use the list.files() function
 list.files()
-# Paths can be specified
-list.files(path = "HW 4")
-# Paths can be specified
-list.files(path = "HW 4" all.files = true)
-# Paths can be specified
+# Paths can be specified. Using the all.files argument shows hidden files.
 list.files(path = "HW 4", all.files = TRUE)
+
+
 # Reading Fixed Width Stuff ----
-library(tidyverse)
-# Reading Fixed Width Stuff ----
+# Use tidyverse
 install.packages("tidyverse")
 library(tidyverse)
+
+# XLSX Files
 install.packages("openxlsx")
+
 # SQL Tables
 library(RMySQL)
-save(sonic, file = "Sonic.R")
+
 # Exporting objects n' stuff
 # Use the save() function.
 sonic <- 5
