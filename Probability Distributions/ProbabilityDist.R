aa41394a911e194f2ce3c95e7c904b0bb98a2327
Franzen <wfranzen@mtech.edu>
2024-04-04 09:16:51 -0600

    Exponential Distributions n' Stuff

--- Probability Distributions/ProbabilityDist.R
+++ Probability Distributions/ProbabilityDist.R
@@ -134,7 +134,107 @@ qnorm(0.8)
 qnorm(0.175)
 
 # Find quartiles
-qnorm(0.75) - qnorm(0.25)
+qnorm(c(0.25, 0.5, 0.75))
 
 # Generate random sample of 1000
-rnorm(1000)
+set.seed(406)
+randomz <- rnorm(1000)
+randomz
+summary(randomz)
+plot(randomz)
+hist(randomz)
+
+# X ~ N (mean = 110, stdev = 13) ----
+m <- 110
+s <- 13
+# Draw a Probability Density Curve from 65 to 155
+plot(65:155, dnorm(65:155, mean = m, sd = s),
+     type = "l",
+     main = expression(paste("Normal Distribution with ", mu, " = 110, ", sigma, " = 13")),
+     xlab = "X",
+     ylab = "f(x)",
+     lwd = 2,
+     col = "blue")
+
+abline(h = 0, col = "gray")
+
+# Find P(X <= 100) and shade this region.
+z <- seq(65, 100, length.out = 300)
+fz <- dnorm(z, mean = m, sd = s)
+polygon(c(65, z, 100), c(0, fz, 0), density=-1, col="magenta")
+
+# Find P(120 <= X <= 135), and shade it.
+z <- seq(120, 135, length.out = 300)
+fz <- dnorm(z, mean = m, sd = s)
+polygon(c(120, z, 135), c(0, fz, 0), density=-1, col="cyan")
+
+text(x= 78, y = 0.017,
+     labels = expression("P(X "<=" 100) = 0.2208782"))
+
+text(x = 140, y = 0.017,
+     labels = expression("P(120 "<=" X < 135) = 0.193643"))
+
+arrows(c(80, 140), c(0.016, 0.016), c(90, 130), c(0.01, 0.01))
+
+# Exponential Distributions ----
+thisrange <- seq(0, 8, length.out = 125)
+plot(thisrange, dexp(thisrange, rate = 1),
+     type = "l",
+     lty = 2,
+     lwd = 3,
+     col = "blue",
+     xlim = c(0.3, 7.8),
+     ylim = c(0, 1.7),
+     xlab = "x",
+     ylab = "f(x)")
+lines(thisrange, dexp(thisrange, rate = 1.65),
+      lty = 1,
+      lwd = 3,
+      col = "red")
+lines(thisrange, dexp(thisrange, rate = 0.4),
+      lty = 3,
+      lwd = 3,
+      col = "green")
+
+legend(6.5, 1.6,
+       legend = c(expression(paste(lambda, " = 1.65")), expression(paste(lambda, " = 1")), expression(paste(lambda, " = 0.4"))),
+       lty = c(1, 2, 3),
+       lwd = 3,
+       col = c("red", "blue", "green"))
+
+# Example Thingy
+# Bridge Thingy; lambda = 1/6
+# Plot the thingy over range (0, 40)
+thisrange <- seq(0, 40, length.out = 150)
+plot(thisrange, dexp(thisrange, 1/6),
+     type = "l",
+     main = expression(paste("Exponential Distribution with ", lambda, " = 1/6")),
+     xlab = "x",
+     ylab = "f(x)",
+     lty = 1,
+     lwd = 2,
+     col = "blue")
+
+# What is the probability of the stress range being:
+# At least 2 MPa
+pexp(2, 1/6, lower.tail = FALSE)
+# At most 7 MPa
+pexp(2, 7)
+# Between 5 and 10 MPa. Shade this one.
+pexp(10, 1/6) - pexp(5, 1/6)
+shadex <- seq(5, 10, length.out = 100)
+shadefx <- dexp(shadex, 1/6)
+polygon(c(5, shadex, 10), c(0, shadefx, 0), density=-1, col="cyan2")
+text(15, 0.1, "P(5 < X < 10) = 0.2457226")
+arrows(10, 0.09, 8, 0.06)
+
+# What value separates the highest 2% of the stress ranges from the remaining 98%?
+qexp(0.98, 1/6)
+
+# Find the median of the distribution
+median(dexp(thisrange, 1/6))
+
+# Find the mean and standard deviation
+mean(dexp(thisrange, 1/6))
+sd(dexp(thisrange, 1/6))
+
